4b0db87298d3aa21dedd4dda01c51492bacd6a67
Kelsey N <kmn9095@utulsa.edu>
2024-03-01 11:42:03 -0600

    Lab6.4

--- Problem#4/filter.py
+++ Problem#4/filter.py
@@ -1,16 +1,19 @@
 def process_and_print(input_string):
-      # Split into separate strings
+    # Split the input string into separate strings
+    numbers = input_string.split()
 
     # Convert strings to integers and filter out negative values
-    input_data = 
+    nums = [int(number) for number in numbers if int(number) < 0]
 
     # Sort integers in reverse order
-  
+    nums.sort(reverse=True)
+
     # Print sorted integers
-    
+    for x in nums:
+        print(x, end=' ')
 
 if __name__ == "__main__":
-    # User inputs string w/ numbers
+    # User inputs a space-separated string of numbers
     user_input = input("Enter a space-separated string of numbers: ")
 
     # Call the function to process and print the result
