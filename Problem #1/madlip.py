cca47aece2b63b07ba1b08f44d677ef1cb556ca0
Kelsey N <kmn9095@utulsa.edu>
2024-03-01 11:30:40 -0600

    Lab6.1

--- Problem #1/madlip.py
+++ Problem #1/madlip.py
@@ -1,9 +1,13 @@
 def food_input():
-    user_input = input()
-    tokens = user_input.split()
-    #type you while  loop here
-
-    
+    while True:
+        user_input = input()
+        tokens = user_input.split()
+        if tokens[0] == "quit":
+            break
+        else:
+            food = tokens[0]
+            quantity = int(tokens[1])
+            print(f"Eating {quantity} {food} a day keeps you happy and healthy.")
 
 if __name__ == "__main__":
     food_input()
